08ecf46171a44a377cdc6e121b32ea52be40fabf
Theresa Pfitzner <pfitzner@outlook.de>
2020-09-19 10:39:07 +0200

    twoway Binding implementiert

--- WPFFahrkartenautomat/WPFFahrkartenautomat/Ticket.cs
+++ WPFFahrkartenautomat/WPFFahrkartenautomat/Ticket.cs
@@ -5,21 +5,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace WPFFahrkartenautomat
 {
-    public class Ticket
+    public class Ticket : INotifyPropertyChanged
     {
         private double _price;
         private Areas _area;
         private string _priceeuro;
+        private string _topay;
+        CultureInfo culture = new CultureInfo("de-DE");
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public Ticket() { }
         public Ticket(Areas ticketarea, double ticketprice)
         {
             this.Area = ticketarea;
             this.Price = ticketprice;
-            string temp = (_price * 100).ToString();
-            _priceeuro = temp.Insert(temp.Length - 2, ",");
         }
 
         public double Price
@@ -28,7 +33,9 @@ namespace WPFFahrkartenautomat
             set
             {
                 _price = value;
-               
+                this.PriceEuro = _price.ToString("c", culture);
+                this.ToPay = "Zu zahlen:\t\t" + this.PriceEuro;
+
             }
         }
 
@@ -41,12 +48,45 @@ namespace WPFFahrkartenautomat
             }
         }
 
+        public string ToPay
+        {
+            get => _topay;
+            set
+            {
+                _topay = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("ToPay"));
+            }
+        }
+
         public Areas Area
         {
             get => _area;
             set
             {
                 _area = value;
+            }
+        }
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        public void TicketBezahlen(double input)
+        {
+            double diff;
+            string output;
+            diff = this.Price - input;
+            output = diff.ToString("c", culture);
+
+            if(diff > 0)
+            {
+                this.ToPay = output.Insert(0, "Zu zahlen: \t\t");
+            }
+            else
+            {
+                this.ToPay =  output.Insert(0, "Wechselgeld: \t\t");
             }
         }
     }
