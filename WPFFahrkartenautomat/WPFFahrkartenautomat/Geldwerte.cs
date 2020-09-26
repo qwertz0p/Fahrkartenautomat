421269f39a13d9f05908a401b08345109ec11033
Theresa Pfitzner <pfitzner@outlook.de>
2020-09-26 22:56:03 +0200

    Geldwerte enum eingefügt

---
+++ WPFFahrkartenautomat/WPFFahrkartenautomat/Geldwerte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFFahrkartenautomat
+{
+    public enum Geldwerte
+    {
+        Cent10 = 10,
+        Cent20 = 20,
+        Cent50 = 50,
+        Euro1 = 100,
+        Euro2 = 200,
+        Euro5 = 500,
+        Euro10 = 1000,
+        Euro20 = 2000
+    }
+}
